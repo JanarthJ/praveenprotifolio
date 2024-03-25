30f7e80e10c26c6bca54e58f082d8a7f5b885203
JanarthJ <janarthananjegadesan@gmail.com>
2024-03-25 12:22:37 +0530

    work

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import "./intro.css";
+
+function Intro() {
+    return (
+      <div className="Intro-MainDiv" style={{marginTop:"80px"}}>
+            <Box component="section">
+                PRAVEEN
+                <p>PRAVEEN</p>
+                PRAVEEN
+            </Box>
+      </div>
+    );
+  }
+  
+  export default Intro;
---
+++ src/components/main/main.js
@@ -0,0 +1,15 @@
+import Header from "../header/header";
+import Intro from "../intro/intro";
+
+
+
+function Main() {
+    return (
+      <div >
+        <Header/>
+        <Intro/>
+      </div>
+    );
+  }
+  
+  export default Main;
